6d05cf196fe7cbfac31a7efee6e030ff602f42fe
Jacob7997 <yakovl1999@gmail.com>
2019-12-11 12:13:52 +0200

    Reverse

--- src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -6,66 +6,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MessageBrokerTest {
-
-    MessageBroker msgB1;
-    MessageBroker msgB2;
-
     @BeforeEach
     public void setUp(){
-        msgB1 =  MessageBrokerImpl.getInstance();
-        msgB2 =  MessageBrokerImpl.getInstance();
+
     }
 
     @Test
     public void test(){
-
+        //TODO: change this test and add more tests :)
+        fail("Not a good test");
     }
-
-    @Test
-    public  void subscribeEvent(Class<? extends Event<T>> type, Subscriber m) {
-    }
-
-    @Test
-    public void subscribeBroadcast(Class<? extends Broadcast> type, Subscriber m) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Test
-    public  void complete(Event<T> e, T result) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Test
-    public void sendBroadcast(Broadcast b) {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Test
-    public void sendEvent() {
-        Future<Integer> futureInteger;
-        msgB1.sendEvent(new IntEvent(5));
-    }
-
-    @Test
-    public void register(Subscriber m) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Test
-    public void unregister(Subscriber m) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Test
-    public void awaitMessage(Subscriber m) throws InterruptedException {
-        // TODO Auto-generated method stub
-    }
-
-
 }
